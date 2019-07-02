341374f77569eeaf4c385f3f59d2ae2aa24d5fb5
napat sangthongthat <52156910+napatnahub@users.noreply.github.com>
2019-07-02 20:22:35 +0700

    Homework: Cin exchange rate

---
+++ Source.cpp
@@ -0,0 +1,28 @@
+# include<iostream>
+# include<string>
+using namespace std;
+int main()
+{
+	string name;
+	
+	float Bath;
+	float Dollar=30.63;
+	float Euro =0.0287;
+	cout << "input Bath is:";
+	cin >> Bath;
+	cout <<   " ****Exchange Rate**** "  << endl;
+	cout << Bath << " Bath is:"<< Bath/Dollar<<"Dollar"<<endl;
+	cout << Bath << " Bath is:" << Bath*Euro << "Euro" << endl;
+	
+	
+	return(0);
+}
+
+	
+	
+
+
+
+
+
+
